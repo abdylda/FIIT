7b6b509a5eee60c65e05fa3339dcf9766d070f1e
abdylda <93abdulla@mail.ru>
2021-01-14 11:44:50 +0600

    first commit

---
+++ cciit/urls.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import login
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+import landing
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+    path('', views.test, name='index'),
+    path('kabinet/', views.kabinet, name='kabinet'),
+    path('Category/', views.category, name='Category'),
+    path('NalichiTehniki/', views.nalichiTehniki, name='NalichiTehniki'),
+    path('NalichiTehnikioitok/', views.nalichiTehnikioitok, name='NalichiTehnikioitok'),
+    path('KlassInjener/', views.klassInjener, name='KlassInjener'),
+    path('Ingener/', views.Ingener, name='Ingener'),
+    path('KlassMebel/', views.klassMebel, name='KlassMebel'),
+    path('kabinetti/koruu/<int:id>', views.kabinet_koruu, name='kabinetti_koruu'),
+    path('categoryny/koruu/<int:id>', views.category_koruu, name='category_koruu'),
+    path('sostoyanieny/koruu/<str:filter>', views.sostoyanieny_koruu, name='sostoyanieny_koruu'),
+
+
+]
